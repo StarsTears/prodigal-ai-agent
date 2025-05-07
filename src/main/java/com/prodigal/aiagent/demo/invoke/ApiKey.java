6d00bc2b2f7bbb0fe8e439e132468b3cb78bb50a
Lang <3510483474@qq.com>
2025-05-07 18:01:00 +0800

    AI Agent author:Lang version:1.0.3 content:将 ApiKey中的key删除

--- src/main/java/com/prodigal/aiagent/demo/invoke/ApiKey.java
+++ src/main/java/com/prodigal/aiagent/demo/invoke/ApiKey.java
@@ -8,5 +8,5 @@ package com.prodigal.aiagent.demo.invoke;
  * @since 2025/4/30
  */
 public class ApiKey {
-    public static final String API_KEY = "sk-ae5197e15145435d85ac9be22f53cd39";
+    public static final String API_KEY = "YOUR  API KEY";
 }
